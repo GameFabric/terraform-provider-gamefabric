31425ba1f6f31643aff9615c63567dd5382a59b7
Christian R <antiphp@users.noreply.github.com>
2025-10-14 09:43:22 +0200

    feat: polish resource environment (GF-1474) (#9)

---
+++ internal/wait/wait.go
@@ -0,0 +1,33 @@
+package wait
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/gamefabric/gf-apiclient/tools/clientset"
+	apierrors "github.com/gamefabric/gf-apicore/api/errors"
+	v1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	"github.com/gamefabric/gf-apicore/runtime"
+)
+
+// We need a timeout for testing.
+const forever = 24 * time.Hour
+
+// PollUntilNotFound polls until the resource is not found.
+func PollUntilNotFound[T runtime.Object](ctx context.Context, getFn clientset.Getter[T], name string) error {
+	bo := backoff.NewExponentialBackOff()
+	bo.MaxElapsedTime = forever // README: Poll forever until the object is gone. We can revisit this decision if necessary.
+
+	return backoff.Retry(func() error {
+		_, err := getFn.Get(ctx, name, v1.GetOptions{})
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return nil
+			}
+			return err
+		}
+		return fmt.Errorf("%q still present", name)
+	}, backoff.WithContext(bo, ctx))
+}
---
+++ internal/wait/wait_test.go
@@ -0,0 +1,74 @@
+package wait_test
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+
+	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	v1 "github.com/gamefabric/gf-core/pkg/api/core/v1"
+	"github.com/gamefabric/gf-core/pkg/apiclient/fake"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/wait"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPollUntilNotFound_Gone(t *testing.T) {
+	t.Parallel()
+
+	cs, err := fake.New()
+	require.NoError(t, err)
+
+	err = wait.PollUntilNotFound(t.Context(), cs.CoreV1().Environments(), "non-existent")
+	require.NoError(t, err)
+}
+
+func TestPollUntilNotFound_NotGone(t *testing.T) {
+	t.Parallel()
+
+	synctest.Test(t, func(t *testing.T) {
+		cs, err := fake.New(&v1.Environment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-env",
+			},
+		})
+		require.NoError(t, err)
+
+		now := time.Now()
+
+		synctest.Wait()
+		err = wait.PollUntilNotFound(t.Context(), cs.CoreV1().Environments(), "test-env")
+
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "still present")
+		assert.GreaterOrEqual(t, time.Since(now), time.Minute)
+	})
+}
+
+func TestPollUntilNotFound_GoneLater(t *testing.T) {
+	t.Parallel()
+
+	synctest.Test(t, func(t *testing.T) {
+		cs, err := fake.New(&v1.Environment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-env",
+			},
+		})
+		require.NoError(t, err)
+
+		now := time.Now()
+
+		go func() {
+			synctest.Wait()
+			<-time.After(time.Minute)
+
+			err := cs.CoreV1().Environments().Delete(t.Context(), "test-env", metav1.DeleteOptions{})
+			require.NoError(t, err)
+		}()
+
+		err = wait.PollUntilNotFound(t.Context(), cs.CoreV1().Environments(), "test-env")
+
+		require.NoError(t, err)
+		assert.GreaterOrEqual(t, time.Since(now), time.Minute)
+	})
+}
