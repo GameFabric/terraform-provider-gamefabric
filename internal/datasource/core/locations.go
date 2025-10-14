f9db6bf88ed5a9e7d800b69b70b7eb9bbb63a451
Nicholas Wiersma <nick@wiersma.co.za>
2025-10-14 11:17:41 +0200

    feat: add locations datasource (#10)

---
+++ internal/datasource/core/locations.go
@@ -0,0 +1,166 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"slices"
+	"strings"
+
+	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	"github.com/gamefabric/gf-core/pkg/apiclient/clientset"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/conv"
+	provcontext "github.com/gamefabric/terraform-provider-gamefabric/internal/provider/context"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ datasource.DataSource              = &locations{}
+	_ datasource.DataSourceWithConfigure = &locations{}
+)
+
+type locations struct {
+	clientSet clientset.Interface
+}
+
+// NewLocations returns a new instance of the locations data source.
+func NewLocations() datasource.DataSource {
+	return &locations{}
+}
+
+func (r *locations) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_locations"
+}
+
+// Schema defines the schema for this data source.
+func (r *locations) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"type": schema.StringAttribute{
+				Description:         "The type used to filter locations.",
+				MarkdownDescription: "The type used to filter locations.",
+				Optional:            true,
+			},
+			"city": schema.StringAttribute{
+				Description:         "The city used to filter locations.",
+				MarkdownDescription: "The city used to filter locations.",
+				Optional:            true,
+			},
+			"country": schema.StringAttribute{
+				Description:         "The country used to filter locations.",
+				MarkdownDescription: "The country used to filter locations.",
+				Optional:            true,
+			},
+			"continent": schema.StringAttribute{
+				Description:         "The continent used to filter locations.",
+				MarkdownDescription: "The continent used to filter locations.",
+				Optional:            true,
+			},
+			"name_regex": schema.StringAttribute{
+				Description:         "The regex used to filter locations by their name.",
+				MarkdownDescription: "The regex used to filter locations by their name.",
+				Optional:            true,
+			},
+			"locations": schema.ListAttribute{
+				Description:         "The locations matching the filters.",
+				MarkdownDescription: "The locations matching the filters.",
+				Computed:            true,
+				ElementType:         types.StringType,
+			},
+		},
+	}
+}
+
+// Configure prepares the struct.
+func (r *locations) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	procCtx, ok := req.ProviderData.(*provcontext.Context)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Provider Data Type",
+			fmt.Sprintf("Expected *provider.Context, got %T", req.ProviderData),
+		)
+		return
+	}
+
+	r.clientSet = procCtx.ClientSet
+}
+
+//nolint:cyclop // Splitting this would not make it much simpler.
+func (r *locations) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var config locationsModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	typ, hasTyp := config.Type.ValueString(), conv.IsKnown(config.Type)
+	city, hasCity := config.City.ValueString(), conv.IsKnown(config.City)
+	country, hasCountry := config.Country.ValueString(), conv.IsKnown(config.Country)
+	continent, hasContinent := config.Continent.ValueString(), conv.IsKnown(config.Continent)
+
+	var (
+		nameRegexp   *regexp.Regexp
+		hasNameRegex bool
+	)
+	if conv.IsKnown(config.NameRegex) {
+		reg, err := regexp.Compile(config.NameRegex.ValueString())
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Error Compiling Name Regex",
+				fmt.Sprintf("Could not compile name regex %q: %v", config.NameRegex.ValueString(), err),
+			)
+			return
+		}
+		nameRegexp = reg
+		hasNameRegex = true
+	}
+
+	list, err := r.clientSet.CoreV1().Locations().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Getting Locations",
+			fmt.Sprintf("Could not get Locations: %v", err),
+		)
+		return
+	}
+
+	locs := make([]string, 0, len(list.Items))
+	for _, loc := range list.Items {
+		annos := loc.GetAnnotations()
+		if annos == nil {
+			annos = map[string]string{}
+		}
+
+		if hasTyp && !strings.EqualFold(annos["g8c.io/provider-type"], typ) {
+			continue
+		}
+		if hasCity && !strings.EqualFold(annos["g8c.io/city"], city) {
+			continue
+		}
+		if hasCountry && !strings.EqualFold(annos["g8c.io/country"], country) {
+			continue
+		}
+		if hasContinent && !strings.EqualFold(annos["g8c.io/continent"], continent) {
+			continue
+		}
+		if hasNameRegex && !nameRegexp.MatchString(loc.Name) {
+			continue
+		}
+		locs = append(locs, loc.Name)
+	}
+	slices.Sort(locs)
+
+	state := newLocationsModel(locs)
+	state.Type = config.Type
+	state.City = config.City
+	state.Country = config.Country
+	state.Continent = config.Continent
+	state.NameRegex = config.NameRegex
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
---
+++ internal/datasource/core/locations_test.go
@@ -0,0 +1,107 @@
+package core_test
+
+import (
+	"testing"
+
+	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	corev1 "github.com/gamefabric/gf-core/pkg/api/core/v1"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/provider/providertest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestLocations(t *testing.T) {
+	loc1 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc-1",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "baremetal",
+				"g8c.io/city":          "amsterdam",
+				"g8c.io/country":       "netherlands",
+				"g8c.io/continent":     "europe",
+			},
+		},
+	}
+	loc2 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc2",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "baremetal",
+				"g8c.io/city":          "amsterdam",
+				"g8c.io/country":       "netherlands",
+				"g8c.io/continent":     "europe",
+			},
+		},
+	}
+	loc3 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc-3",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "something",
+				"g8c.io/city":          "amsterdam",
+				"g8c.io/country":       "netherlands",
+				"g8c.io/continent":     "europe",
+			},
+		},
+	}
+	loc4 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc-4",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "baremetal",
+				"g8c.io/city":          "something",
+				"g8c.io/country":       "netherlands",
+				"g8c.io/continent":     "europe",
+			},
+		},
+	}
+	loc5 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc-5",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "baremetal",
+				"g8c.io/city":          "amsterdam",
+				"g8c.io/country":       "something",
+				"g8c.io/continent":     "europe",
+			},
+		},
+	}
+	loc6 := &corev1.Location{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "loc-6",
+			Annotations: map[string]string{
+				"g8c.io/provider-type": "baremetal",
+				"g8c.io/city":          "amsterdam",
+				"g8c.io/country":       "netherlands",
+				"g8c.io/continent":     "something",
+			},
+		},
+	}
+
+	pf, _ := providertest.ProtoV6ProviderFactories(t, loc1, loc2, loc3, loc4, loc5, loc6)
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		ProtoV6ProviderFactories: pf,
+		Steps: []resource.TestStep{
+			{
+				Config: `data "gamefabric_locations" "test1" {
+  type = "Baremetal"
+  city = "Amsterdam"
+  country = "Netherlands"
+  continent = "Europe"
+  name_regex = "loc-.*"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "type", "Baremetal"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "city", "Amsterdam"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "country", "Netherlands"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "continent", "Europe"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "name_regex", "loc-.*"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "locations.#", "1"),
+					resource.TestCheckResourceAttr("data.gamefabric_locations.test1", "locations.0", "loc-1"),
+				),
+			},
+		},
+	})
+}
