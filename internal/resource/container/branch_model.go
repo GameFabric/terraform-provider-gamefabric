68e2ddda8d22e305d0b7d032c4934d5b92127d5f
Rafał Mnich <rafal@futurehome.no>
2025-10-15 15:30:27 +0200

    feat: add branch resource with schema and CRUD operations (#17)

---
+++ internal/resource/container/branch_model.go
@@ -0,0 +1,70 @@
+package container
+
+import (
+	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	containerv1 "github.com/gamefabric/gf-core/pkg/api/container/v1"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/conv"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// branchModel is the model for a branch resource.
+type branchModel struct {
+	ID                   types.String                          `tfsdk:"id"`
+	Name                 types.String                          `tfsdk:"name"`
+	Labels               map[string]types.String               `tfsdk:"labels"`
+	DisplayName          types.String                          `tfsdk:"display_name"`
+	Description          types.String                          `tfsdk:"description"`
+	RetentionPolicyRules []branchImageRetentionPolicyRuleModel `tfsdk:"retention_policy_rules"`
+}
+
+// branchImageRetentionPolicyRuleModel is the model for an image retention policy rule.
+type branchImageRetentionPolicyRuleModel struct {
+	Name       types.String `tfsdk:"name"`
+	ImageRegex types.String `tfsdk:"image_regex"`
+	TagRegex   types.String `tfsdk:"tag_regex"`
+	KeepCount  types.Int64  `tfsdk:"keep_count"`
+	KeepDays   types.Int64  `tfsdk:"keep_days"`
+}
+
+func newBranchModel(obj *containerv1.Branch) branchModel {
+	return branchModel{
+		ID:          types.StringValue(obj.Name),
+		Name:        types.StringValue(obj.Name),
+		Labels:      conv.ForEachMapItem(obj.Labels, func(item string) types.String { return types.StringValue(item) }),
+		DisplayName: types.StringValue(obj.Spec.DisplayName),
+		Description: conv.OptionalFunc(obj.Spec.Description, types.StringValue, types.StringNull),
+		RetentionPolicyRules: conv.ForEachSliceItem(obj.Spec.RetentionPolicyRules, func(item containerv1.BranchImageRetentionPolicyRule) branchImageRetentionPolicyRuleModel {
+			return branchImageRetentionPolicyRuleModel{
+				Name:       types.StringValue(item.Name),
+				ImageRegex: types.StringValue(item.ImageRegex),
+				TagRegex:   types.StringValue(item.TagRegex),
+				KeepCount:  types.Int64Value(int64(item.KeepCount)),
+				KeepDays:   types.Int64Value(int64(item.KeepDays)),
+			}
+		}),
+	}
+}
+
+func (m branchModel) ToObject() *containerv1.Branch {
+	return &containerv1.Branch{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   m.Name.ValueString(),
+			Labels: conv.ForEachMapItem(m.Labels, func(item types.String) string { return item.ValueString() }),
+		},
+		Spec: containerv1.BranchSpec{
+			DisplayName:          m.DisplayName.ValueString(),
+			Description:          m.Description.ValueString(),
+			RetentionPolicyRules: conv.ForEachSliceItem(m.RetentionPolicyRules, retentionFromModel),
+		},
+	}
+}
+
+func retentionFromModel(rule branchImageRetentionPolicyRuleModel) containerv1.BranchImageRetentionPolicyRule {
+	return containerv1.BranchImageRetentionPolicyRule{
+		Name:       rule.Name.ValueString(),
+		ImageRegex: rule.ImageRegex.ValueString(),
+		TagRegex:   rule.TagRegex.ValueString(),
+		KeepCount:  int(rule.KeepCount.ValueInt64()),
+		KeepDays:   int(rule.KeepDays.ValueInt64()),
+	}
+}
---
+++ internal/resource/container/branch_test.go
@@ -0,0 +1,86 @@
+package container_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/provider/providertest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestBranch(t *testing.T) {
+	name := "dflt"
+	pf, _ := providertest.ProtoV6ProviderFactories(t)
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		ProtoV6ProviderFactories: pf,
+		Steps: []resource.TestStep{
+			{
+				Config: testResourceBranchConfigBasic(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "name", name),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "display_name", "My Branch"),
+				),
+			},
+			{
+				ResourceName:      "gamefabric_branch.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testResourceBranchConfigBasicWithDescription(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "name", name),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "display_name", "My Branch"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "description", "My Branch Description"),
+				),
+			},
+			{
+				Config: testResourceBranchConfigWithRetentionPolicy(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "name", name),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "display_name", "My Branch"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.#", "1"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.0.name", "rule1"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.0.image_regex", ".*"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.0.tag_regex", ".*"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.0.keep_count", "10"),
+					resource.TestCheckResourceAttr("gamefabric_branch.test", "retention_policy_rules.0.keep_days", "30"),
+				),
+			},
+		},
+	})
+}
+
+func testResourceBranchConfigBasic(name string) string {
+	return fmt.Sprintf(`resource "gamefabric_branch" "test" {
+  name = "%s"
+  display_name = "My Branch"
+}`, name)
+}
+
+func testResourceBranchConfigBasicWithDescription(name string) string {
+	return fmt.Sprintf(`resource "gamefabric_branch" "test" {
+  name = "%s"
+  display_name = "My Branch"
+  description = "My Branch Description"
+}`, name)
+}
+
+func testResourceBranchConfigWithRetentionPolicy(name string) string {
+	return fmt.Sprintf(`resource "gamefabric_branch" "test" {
+  name = "%s"
+  display_name = "My Branch"
+  description = "My Branch Description"
+  retention_policy_rules = [
+	{
+	  name = "rule1"
+      keep_count = 10
+      keep_days = 30
+      image_regex = ".*"
+	  tag_regex = ".*"
+	}
+  ]
+}`, name)
+}
