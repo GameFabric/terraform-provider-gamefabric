6528b35c705797813f5f34cc0c43bd7a54a9d4a0
Nicholas Wiersma <nick@wiersma.co.za>
2025-10-16 13:30:30 +0200

    fix: branch models

--- internal/resource/container/branch_model.go
+++ internal/resource/container/branch_model.go
@@ -7,17 +7,47 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// branchModel is the model for a branch resource.
 type branchModel struct {
 	ID                   types.String                          `tfsdk:"id"`
 	Name                 types.String                          `tfsdk:"name"`
 	Labels               map[string]types.String               `tfsdk:"labels"`
+	Annotations          map[string]types.String               `tfsdk:"annotations"`
 	DisplayName          types.String                          `tfsdk:"display_name"`
 	Description          types.String                          `tfsdk:"description"`
 	RetentionPolicyRules []branchImageRetentionPolicyRuleModel `tfsdk:"retention_policy_rules"`
 }
 
-// branchImageRetentionPolicyRuleModel is the model for an image retention policy rule.
+func newBranchModel(obj *containerv1.Branch) branchModel {
+	return branchModel{
+		ID:                   types.StringValue(obj.Name),
+		Name:                 types.StringValue(obj.Name),
+		Labels:               conv.ForEachMapItem(obj.Labels, func(item string) types.String { return types.StringValue(item) }),
+		Annotations:          conv.ForEachMapItem(obj.Annotations, func(item string) types.String { return types.StringValue(item) }),
+		DisplayName:          types.StringValue(obj.Spec.DisplayName),
+		Description:          conv.OptionalFunc(obj.Spec.Description, types.StringValue, types.StringNull),
+		RetentionPolicyRules: emptyIfNil(conv.ForEachSliceItem(obj.Spec.RetentionPolicyRules, newBranchImageRetentionPolicyRuleModel)),
+	}
+}
+
+func (m branchModel) ToObject() *containerv1.Branch {
+	return &containerv1.Branch{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        m.Name.ValueString(),
+			Labels:      conv.ForEachMapItem(m.Labels, func(item types.String) string { return item.ValueString() }),
+			Annotations: conv.ForEachMapItem(m.Annotations, func(item types.String) string { return item.ValueString() }),
+		},
+		Spec: containerv1.BranchSpec{
+			DisplayName: m.DisplayName.ValueString(),
+			Description: m.Description.ValueString(),
+			RetentionPolicyRules: conv.ForEachSliceItem(m.RetentionPolicyRules,
+				func(item branchImageRetentionPolicyRuleModel) containerv1.BranchImageRetentionPolicyRule {
+					return item.ToObject()
+				},
+			),
+		},
+	}
+}
+
 type branchImageRetentionPolicyRuleModel struct {
 	Name       types.String `tfsdk:"name"`
 	ImageRegex types.String `tfsdk:"image_regex"`
@@ -26,46 +56,23 @@ type branchImageRetentionPolicyRuleModel struct {
 	KeepDays   types.Int64  `tfsdk:"keep_days"`
 }
 
-func newBranchModel(obj *containerv1.Branch) branchModel {
-	return branchModel{
-		ID:          types.StringValue(obj.Name),
-		Name:        types.StringValue(obj.Name),
-		Labels:      conv.ForEachMapItem(obj.Labels, func(item string) types.String { return types.StringValue(item) }),
-		DisplayName: types.StringValue(obj.Spec.DisplayName),
-		Description: conv.OptionalFunc(obj.Spec.Description, types.StringValue, types.StringNull),
-		RetentionPolicyRules: emptyIfNil(conv.ForEachSliceItem(obj.Spec.RetentionPolicyRules, func(item containerv1.BranchImageRetentionPolicyRule) branchImageRetentionPolicyRuleModel {
-			return branchImageRetentionPolicyRuleModel{
-				Name:       types.StringValue(item.Name),
-				ImageRegex: types.StringValue(item.ImageRegex),
-				TagRegex:   types.StringValue(item.TagRegex),
-				KeepCount:  types.Int64Value(int64(item.KeepCount)),
-				KeepDays:   types.Int64Value(int64(item.KeepDays)),
-			}
-		})),
+func newBranchImageRetentionPolicyRuleModel(rule containerv1.BranchImageRetentionPolicyRule) branchImageRetentionPolicyRuleModel {
+	return branchImageRetentionPolicyRuleModel{
+		Name:       types.StringValue(rule.Name),
+		ImageRegex: types.StringValue(rule.ImageRegex),
+		TagRegex:   types.StringValue(rule.TagRegex),
+		KeepCount:  types.Int64Value(int64(rule.KeepCount)),
+		KeepDays:   types.Int64Value(int64(rule.KeepDays)),
 	}
 }
 
-func (m branchModel) ToObject() *containerv1.Branch {
-	return &containerv1.Branch{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   m.Name.ValueString(),
-			Labels: conv.ForEachMapItem(m.Labels, func(item types.String) string { return item.ValueString() }),
-		},
-		Spec: containerv1.BranchSpec{
-			DisplayName:          m.DisplayName.ValueString(),
-			Description:          m.Description.ValueString(),
-			RetentionPolicyRules: conv.ForEachSliceItem(m.RetentionPolicyRules, retentionFromModel),
-		},
-	}
-}
-
-func retentionFromModel(rule branchImageRetentionPolicyRuleModel) containerv1.BranchImageRetentionPolicyRule {
+func (m branchImageRetentionPolicyRuleModel) ToObject() containerv1.BranchImageRetentionPolicyRule {
 	return containerv1.BranchImageRetentionPolicyRule{
-		Name:       rule.Name.ValueString(),
-		ImageRegex: rule.ImageRegex.ValueString(),
-		TagRegex:   rule.TagRegex.ValueString(),
-		KeepCount:  int(rule.KeepCount.ValueInt64()),
-		KeepDays:   int(rule.KeepDays.ValueInt64()),
+		Name:       m.Name.ValueString(),
+		ImageRegex: m.ImageRegex.ValueString(),
+		TagRegex:   m.TagRegex.ValueString(),
+		KeepCount:  int(m.KeepCount.ValueInt64()),
+		KeepDays:   int(m.KeepDays.ValueInt64()),
 	}
 }
 
