29d60d4ef7e0dcc7c69a0fd000395b3a7d163a32
Rafal Mnich <rafal.mnich@nitrado.net>
2025-10-24 13:54:58 +0200

    feat: enhance volumestore data source to support optional name and region attributes

--- internal/datasource/storage/volumestore_test.go
+++ internal/datasource/storage/volumestore_test.go
@@ -1,6 +1,7 @@
 package storage_test
 
 import (
+	"regexp"
 	"testing"
 
 	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
@@ -11,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestVolumeStore(t *testing.T) {
+func TestVolumeStore_ByName(t *testing.T) {
 	t.Parallel()
 
 	vs := &storagev1.VolumeStore{
@@ -32,7 +33,6 @@ func TestVolumeStore(t *testing.T) {
 			{
 				Config: `data "gamefabric_volumestore" "test" {
   name = "test-volumestore"
-  region = "europe-west1"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -40,6 +40,76 @@ func TestVolumeStore(t *testing.T) {
 					resource.TestCheckResourceAttr("data.gamefabric_volumestore.test", "region", "europe-west1"),
 					resource.TestCheckResourceAttr("data.gamefabric_volumestore.test", "max_volume_size", "10Mi"),
 				),
+			},
+		},
+	})
+}
+
+func TestVolumeStore_ByRegion(t *testing.T) {
+	t.Parallel()
+
+	vs := &storagev1.VolumeStore{
+		ObjectMeta: metav1.ObjectMeta{Name: "vs-region"},
+		Spec: storagev1.VolumeStoreSpec{
+			Region:        "europe-west1",
+			Destination:   "gs://bucket2",
+			MaxVolumeSize: *conv.Quantity(typesframework.StringValue("5Gi")),
+		},
+	}
+
+	pf, _ := providertest.ProtoV6ProviderFactories(t, vs)
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		ProtoV6ProviderFactories: pf,
+		Steps: []resource.TestStep{
+			{
+				Config: `data "gamefabric_volumestore" "by_region" {
+  region = "europe-west1"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.gamefabric_volumestore.by_region", "name", "vs-region"),
+					resource.TestCheckResourceAttr("data.gamefabric_volumestore.by_region", "region", "europe-west1"),
+					resource.TestCheckResourceAttr("data.gamefabric_volumestore.by_region", "max_volume_size", "5Gi"),
+				),
+			},
+		},
+	})
+}
+
+func TestVolumeStore_ByRegion_MultipleMatches(t *testing.T) {
+	t.Parallel()
+
+	vs1 := &storagev1.VolumeStore{
+		ObjectMeta: metav1.ObjectMeta{Name: "vs-a"},
+		Spec: storagev1.VolumeStoreSpec{
+			Region:        "dup-region",
+			Destination:   "gs://b1",
+			MaxVolumeSize: *conv.Quantity(typesframework.StringValue("1Gi")),
+		},
+	}
+	vs2 := &storagev1.VolumeStore{
+		ObjectMeta: metav1.ObjectMeta{Name: "vs-b"},
+		Spec: storagev1.VolumeStoreSpec{
+			Region:        "dup-region",
+			Destination:   "gs://b2",
+			MaxVolumeSize: *conv.Quantity(typesframework.StringValue("2Gi")),
+		},
+	}
+
+	pf, _ := providertest.ProtoV6ProviderFactories(t, vs1, vs2)
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		ProtoV6ProviderFactories: pf,
+		Steps: []resource.TestStep{
+			{
+				Config: `data "gamefabric_volumestore" "dup" {
+  region = "dup-region"
+}
+`,
+				ExpectError: regexp.MustCompile(`Multiple VolumeStores Found`),
 			},
 		},
 	})
