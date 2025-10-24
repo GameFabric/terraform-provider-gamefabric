8eda817b26bd09e3db69af2f0b81b8da9065f678
Georg Doser <georg.doser@nitrado.net>
2025-10-24 17:12:10 +0200

    document image datasource

---
+++ examples/data-sources/gamefabric_image/specific-image-tag.tf
@@ -0,0 +1,19 @@
+data "gamefabric_image" "gameserver" {
+  branch = data.gamefabric_branch.example.name
+  image  = "gameserver"
+  tag    = "0.4.0"
+}
+
+resource "gamefabric_armada" "this" {
+  name        = "myarmada"
+  environment = data.gamefabric_environment.prod.name
+
+  region = data.gamefabric_region.europe.name
+  replicas = [...]
+  containers = [
+    {
+      name  = "default" # the gameserver container should always be named "default"
+      image = data.gamefabric_image.gameserver.image_target
+    }
+  ]
+}
