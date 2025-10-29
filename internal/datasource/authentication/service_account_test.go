cbb3181e608273435a0e0cfd8a1aadc0a27d13eb
Rafal Mnich <rafal.mnich@nitrado.net>
2025-10-29 13:46:29 +0100

    fixes

--- internal/datasource/authentication/service_account_test.go
+++ internal/datasource/authentication/service_account_test.go
@@ -1,7 +1,6 @@
 package authentication_test
 
 import (
-	"regexp"
 	"testing"
 
 	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
@@ -15,12 +14,12 @@ func TestServiceAccount(t *testing.T) {
 
 	sa := &authv1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   "user",
+			Name:   "test-sa",
 			Labels: map[string]string{"env": "test", "team": "devops"},
 		},
 		Spec: authv1.ServiceAccountSpec{
-			Username: "user",
-			Email:    "user@example.com",
+			Username: "svc-user",
+			Email:    "svc@example.com",
 		},
 		Status: authv1.ServiceAccountStatus{
 			State:    authv1.ServiceAccountStateApplied,
@@ -30,39 +29,24 @@ func TestServiceAccount(t *testing.T) {
 
 	pf, _ := providertest.ProtoV6ProviderFactories(t, sa)
 
-	checkServiceAccount := func(resourceName string) resource.TestCheckFunc {
-		return resource.ComposeAggregateTestCheckFunc(
-			resource.TestCheckResourceAttr(resourceName, "name", "user"),
-			resource.TestCheckResourceAttr(resourceName, "email", "user@example.com"),
-			resource.TestCheckResourceAttr(resourceName, "labels.env", "test"),
-			resource.TestCheckResourceAttr(resourceName, "labels.team", "devops"),
-		)
-	}
-
 	resource.Test(t, resource.TestCase{
 		IsUnitTest:               true,
 		ProtoV6ProviderFactories: pf,
 		Steps: []resource.TestStep{
 			{
-				Config: `data "gamefabric_service_account" "this" {
-  name = "user"
+				Config: `data "gamefabric_service_account" "test-sa" {
+  name = "test-sa"
 }
 `,
-				Check: checkServiceAccount("data.gamefabric_service_account.this"),
-			},
-			{
-				Config: `data "gamefabric_service_account" "this" {
-  email = "user@example.com"
-}
-`,
-				Check: checkServiceAccount("data.gamefabric_service_account.this"),
-			},
-			{
-				Config: `data "gamefabric_service_account" "this" {
-  name = "nonexistent"
-}
-`,
-				ExpectError: regexp.MustCompile(`Failed to retrieve ServiceAccount`),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "name", "test-sa"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "username", "svc-user"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "email", "svc@example.com"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "state", "Applied"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "password", "s3cr3t"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "labels.env", "test"),
+					resource.TestCheckResourceAttr("data.gamefabric_service_account.test-sa", "labels.team", "devops"),
+				),
 			},
 		},
 	})
