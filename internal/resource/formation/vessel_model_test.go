ca329a9262a89a1a639bf136aaecd2bbf5c81f79
Christian Reinecke <christian.reinecke@nitrado.net>
2025-11-05 15:29:20 +0100

    fix: review

--- internal/resource/formation/vessel_model_test.go
+++ internal/resource/formation/vessel_model_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gamefabric/terraform-provider-gamefabric/internal/resource/container"
 	"github.com/gamefabric/terraform-provider-gamefabric/internal/resource/core"
 	"github.com/gamefabric/terraform-provider-gamefabric/internal/resource/mps"
+	"github.com/hamba/pkg/v2/ptr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stretchr/testify/assert"
 	kcorev1 "k8s.io/api/core/v1"
@@ -39,10 +40,12 @@ var (
 			},
 		},
 		Spec: formationv1.VesselSpec{
+
 			Description: "Test Vessel Description",
 			Region:      "us-west",
-			Suspend:     boolPtr(true),
+			Suspend:     ptr.Of(true),
 			Template: formationv1.GameServerTemplateSpec{
+
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
 						"gameserver-label-key": "gameserver-label-value",
@@ -58,7 +61,7 @@ var (
 						"gateway-policy-2",
 					},
 					Health: agonesv1.Health{
-						Disabled:            false,
+						Disabled:            true,
 						InitialDelaySeconds: int32(10),
 						PeriodSeconds:       int32(5),
 						FailureThreshold:    int32(3),
@@ -77,7 +80,7 @@ var (
 							},
 							Resources: kcorev1.ResourceRequirements{
 								Limits: kcorev1.ResourceList{
-									kcorev1.ResourceCPU:    resource.MustParse("1000m"),
+									kcorev1.ResourceCPU:    resource.MustParse("800m"),
 									kcorev1.ResourceMemory: resource.MustParse("512Mi"),
 								},
 								Requests: kcorev1.ResourceList{
@@ -91,15 +94,16 @@ var (
 									Value: "value1",
 								},
 							},
-							Ports: []formationv1.ContainerPort{
+							Ports: []formationv1.Port{
 								{
-									Name:          "game-port",
-									ContainerPort: int32(7777),
-									Protocol:      kcorev1.ProtocolUDP,
-									HostPort:      int32(0),
+									Name:               "game-port",
+									Policy:             "Passthrough",
+									ContainerPort:      7777,
+									Protocol:           kcorev1.ProtocolUDP,
+									ProtectionProtocol: ptr.Of("example-protocol"),
 								},
 							},
-							VolumeMounts: []corev1.VolumeMount{
+							VolumeMounts: []kcorev1.VolumeMount{
 								{
 									Name:      "data-volume",
 									MountPath: "/data",
@@ -107,21 +111,19 @@ var (
 							},
 							ConfigFiles: []formationv1.ConfigFileMount{
 								{
-									Name:        "config-file-1",
-									MountPath:   "/config/file1.conf",
-									Content:     "config content",
-									Permissions: "0644",
+									Name:      "config-file-1",
+									MountPath: "/config/file1.conf",
 								},
 							},
 						},
 					},
-					TerminationGracePeriodSeconds: int64Ptr(30),
+					TerminationGracePeriodSeconds: ptr.Of[int64](30),
 					Volumes: []formationv1.Volume{
 						{
 							Name: "data-volume",
 							Type: formationv1.VolumeTypeEmptyDir,
 							EmptyDir: &formationv1.EmptyDirVolumeSource{
-								SizeLimit: resource.MustParse("1Gi"),
+								SizeLimit: ptr.Of(resource.MustParse("1Gi")),
 							},
 						},
 						{
@@ -135,9 +137,9 @@ var (
 				},
 			},
 			TerminationGracePeriods: &formationv1.VesselTerminationGracePeriods{
-				Maintenance:   int64Ptr(60),
-				SpecChange:    int64Ptr(45),
-				UserInitiated: int64Ptr(90),
+				Maintenance:   ptr.Of[int64](60),
+				SpecChange:    ptr.Of[int64](45),
+				UserInitiated: ptr.Of[int64](90),
 			},
 		},
 	}
@@ -148,7 +150,7 @@ var (
 		Environment: types.StringValue("test-env"),
 		Region:      types.StringValue("us-west"),
 		Description: types.StringValue("Test Vessel Description"),
-		Suspend:     types.BoolPointerValue(boolPtr(true)),
+		Suspend:     types.BoolPointerValue(ptr.Of(true)),
 		Labels: map[string]types.String{
 			"label-key": types.StringValue("label-value"),
 		},
@@ -165,9 +167,8 @@ var (
 			types.StringValue("gateway-policy-1"),
 			types.StringValue("gateway-policy-2"),
 		},
-		ProfilingEnabled: types.BoolValue(true),
 		HealthChecks: &mps.HealthChecksModel{
-			Disabled:            types.BoolValue(false),
+			Disabled:            types.BoolValue(true),
 			InitialDelaySeconds: types.Int32Value(10),
 			PeriodSeconds:       types.Int32Value(5),
 			FailureThreshold:    types.Int32Value(3),
@@ -187,11 +188,11 @@ var (
 					types.StringValue("echo hello"),
 				},
 				Resources: &mps.ResourcesModel{
-					Limits: &mps.ResourceModel{
-						CPU:    types.StringValue("1000m"),
+					Limits: &mps.ResourceSpecModel{
+						CPU:    types.StringValue("800m"),
 						Memory: types.StringValue("512Mi"),
 					},
-					Requests: &mps.ResourceModel{
+					Requests: &mps.ResourceSpecModel{
 						CPU:    types.StringValue("500m"),
 						Memory: types.StringValue("256Mi"),
 					},
@@ -204,10 +205,11 @@ var (
 				},
 				Ports: []mps.PortModel{
 					{
-						Name:          types.StringValue("game-port"),
-						ContainerPort: types.Int32Value(7777),
-						Protocol:      types.StringValue("UDP"),
-						HostPort:      types.Int32Value(0),
+						Name:               types.StringValue("game-port"),
+						Protocol:           types.StringValue("UDP"),
+						ContainerPort:      types.Int32Value(7777),
+						Policy:             types.StringValue("Passthrough"),
+						ProtectionProtocol: types.StringValue("example-protocol"),
 					},
 				},
 				VolumeMounts: []mps.VolumeMountModel{
@@ -218,19 +220,17 @@ var (
 				},
 				ConfigFiles: []mps.ConfigFileModel{
 					{
-						Name:        types.StringValue("config-file-1"),
-						MountPath:   types.StringValue("/config/file1.conf"),
-						Content:     types.StringValue("config content"),
-						Permissions: types.StringValue("0644"),
+						Name:      types.StringValue("config-file-1"),
+						MountPath: types.StringValue("/config/file1.conf"),
 					},
 				},
 			},
 		},
 		TerminationConfig: &terminationConfigModel{
-			GracePeriod:   types.Int64PointerValue(int64Ptr(30)),
-			Maintenance:   types.Int64PointerValue(int64Ptr(60)),
-			SpecChange:    types.Int64PointerValue(int64Ptr(45)),
-			UserInitiated: types.Int64PointerValue(int64Ptr(90)),
+			GracePeriod:   types.Int64PointerValue(ptr.Of[int64](30)),
+			Maintenance:   types.Int64PointerValue(ptr.Of[int64](60)),
+			SpecChange:    types.Int64PointerValue(ptr.Of[int64](45)),
+			UserInitiated: types.Int64PointerValue(ptr.Of[int64](90)),
 		},
 		Volumes: []volumeModel{
 			{
@@ -246,6 +246,7 @@ var (
 				},
 			},
 		},
+		ProfilingEnabled: types.BoolValue(true),
 		ImageUpdaterTarget: container.NewImageUpdaterTargetModel(
 			container.ImageUpdaterTargetTypeVessel,
 			"test-vessel",
@@ -253,11 +254,3 @@ var (
 		),
 	}
 )
-
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func int64Ptr(i int64) *int64 {
-	return &i
-}
