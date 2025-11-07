411ee7ce0c9a3a7e719810a8428dfbfd628d020c
Nicholas Wiersma <nick@wiersma.co.za>
2025-11-07 13:25:23 +0200

    feat: add group resource (#108)

---
+++ internal/resource/rbac/group.go
@@ -0,0 +1,222 @@
+package rbac
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gamefabric/gf-apiclient/rest"
+	"github.com/gamefabric/gf-apiclient/tools/patch"
+	apierrors "github.com/gamefabric/gf-apicore/api/errors"
+	metav1 "github.com/gamefabric/gf-apicore/apis/meta/v1"
+	"github.com/gamefabric/gf-core/pkg/apiclient/clientset"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/normalize"
+	provcontext "github.com/gamefabric/terraform-provider-gamefabric/internal/provider/context"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/validators"
+	"github.com/gamefabric/terraform-provider-gamefabric/internal/wait"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource                = &group{}
+	_ resource.ResourceWithConfigure   = &group{}
+	_ resource.ResourceWithImportState = &group{}
+)
+
+type group struct {
+	clientSet clientset.Interface
+}
+
+// NewGroup returns a new group resource.
+func NewGroup() resource.Resource {
+	return &group{}
+}
+
+// Metadata sets the resource type name.
+func (r *group) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_group"
+}
+
+// Schema defines the schema for this resource.
+func (r *group) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description:         "The unique Terraform identifier.",
+				MarkdownDescription: "The unique Terraform identifier.",
+				Computed:            true,
+			},
+			"name": schema.StringAttribute{
+				Description:         "The unique object name within its scope. Must contain only lowercase alphanumeric characters, hyphens, or dots. Must start and end with an alphanumeric character. Maximum length is 63 characters.",
+				MarkdownDescription: "The unique object name within its scope. Must contain only lowercase alphanumeric characters, hyphens, or dots. Must start and end with an alphanumeric character. Maximum length is 63 characters.",
+				Required:            true,
+				Validators: []validator.String{
+					validators.NameValidator{},
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"labels": schema.MapAttribute{
+				Description:         "A map of keys and values that can be used to organize and categorize objects.",
+				MarkdownDescription: "A map of keys and values that can be used to organize and categorize objects.",
+				Optional:            true,
+				ElementType:         types.StringType,
+				Validators: []validator.Map{
+					validators.LabelsValidator{},
+				},
+			},
+			"annotations": schema.MapAttribute{
+				Description:         "Annotations is an unstructured map of keys and values stored on an object.",
+				MarkdownDescription: "Annotations is an unstructured map of keys and values stored on an object.",
+				Optional:            true,
+				ElementType:         types.StringType,
+				Validators: []validator.Map{
+					validators.AnnotationsValidator{},
+				},
+			},
+			"users": schema.ListAttribute{
+				Description:         "The users that are part of the group.",
+				MarkdownDescription: "The users that are part of the group.",
+				Optional:            true,
+				ElementType:         types.StringType,
+			},
+		},
+	}
+}
+
+// Configure prepares the struct.
+func (r *group) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	procCtx, ok := req.ProviderData.(*provcontext.Context)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Provider Data Type",
+			fmt.Sprintf("Expected *provider.Context, got %T", req.ProviderData),
+		)
+		return
+	}
+
+	r.clientSet = procCtx.ClientSet
+}
+
+func (r *group) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan groupModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	obj := plan.ToObject()
+	outObj, err := r.clientSet.RBACV1().Groups().Create(ctx, obj, metav1.CreateOptions{})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Creating Group",
+			fmt.Sprintf("Could not create Group: %v", err),
+		)
+		return
+	}
+
+	plan = newGroupModel(outObj)
+	resp.Diagnostics.Append(normalize.Model(ctx, &plan, req.Plan)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (r *group) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state groupModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	obj, err := r.clientSet.RBACV1().Groups().Get(ctx, state.Name.ValueString(), metav1.GetOptions{})
+	if err != nil {
+		switch {
+		case apierrors.IsNotFound(err):
+			resp.State.RemoveResource(ctx)
+		default:
+			resp.Diagnostics.AddError(
+				"Error Reading Group",
+				fmt.Sprintf("Could not read Group %q: %v", state.Name.ValueString(), err),
+			)
+		}
+		return
+	}
+
+	state = newGroupModel(obj)
+	resp.Diagnostics.Append(normalize.Model(ctx, &state, req.State)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *group) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan, state groupModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	oldObj := state.ToObject()
+	newObj := plan.ToObject()
+
+	pb, err := patch.Create(oldObj, newObj)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Creating Group Patch",
+			fmt.Sprintf("Could not create patch for Group: %v", err),
+		)
+		return
+	}
+
+	if _, err = r.clientSet.RBACV1().Groups().Patch(ctx, newObj.Name, rest.MergePatchType, pb, metav1.UpdateOptions{}); err != nil {
+		resp.Diagnostics.AddError(
+			"Error Patching Group",
+			fmt.Sprintf("Could not patch Group: %v", err),
+		)
+		return
+	}
+
+	plan.ID = types.StringValue(newObj.Name)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (r *group) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state groupModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := r.clientSet.RBACV1().Groups().Delete(ctx, state.Name.ValueString(), metav1.DeleteOptions{})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Deleting Group",
+			fmt.Sprintf("Could not delete Group: %v", err),
+		)
+		return
+	}
+
+	err = wait.PollUntilNotFound(ctx, r.clientSet.RBACV1().Groups(), state.Name.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Waiting for Group Deletion",
+			fmt.Sprintf("Timed out waiting for deletion of Group: %v", err),
+		)
+	}
+}
+
+func (r *group) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
+}
