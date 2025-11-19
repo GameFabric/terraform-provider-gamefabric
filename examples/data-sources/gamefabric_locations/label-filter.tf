c7231c4d2f08a6df3fb96722a0b1e8330a6674ef
Christian R <antiphp@users.noreply.github.com>
2025-11-19 11:17:41 +0100

    Apply suggestions from code review
    Co-authored-by: Blex <15048849+BlexToGo@users.noreply.github.com>

--- examples/data-sources/gamefabric_locations/label-filter.tf
+++ examples/data-sources/gamefabric_locations/label-filter.tf
@@ -1,14 +1,17 @@
-data "gamefabric_locations" "us-east" {
+data "gamefabric_locations" "us_east" {
   label_filter = {
     "g8c.io/country" = "us"
     "g8c.io/state" = [
-      "sc", # South Carolina, us-east1
-      "oh", # Ohio, us-east5
+      "sc", # South Carolina
+      "oh", # Ohio
+      "fl", # Florida
+      "ny", # New York
+      "dc", # District of Columbia
     ]
   }
 }
 
-data "gamefabric_locations" "eu-west" {
+data "gamefabric_locations" "eu_west" {
   continent = "europe"
   label_filter = {
     "g8c.io/city" = ["amsterdam", "frankfurt", "london"]
