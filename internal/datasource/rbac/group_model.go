d9f770af76dc5b605cade95a91d8d6fa6f336645
Rafal Mnich <rafal.mnich@nitrado.net>
2026-01-02 11:52:46 +0100

    chore: bump dependencies (GF-1614)

--- internal/datasource/rbac/group_model.go
+++ internal/datasource/rbac/group_model.go
@@ -1,16 +1,9 @@
 package rbac
 
 import (
-	rbacv1 "github.com/gamefabric/gf-core/pkg/api/rbac/v1"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type groupModel struct {
 	Name types.String `tfsdk:"name"`
-}
-
-func newGroupModel(obj *rbacv1.Group) groupModel {
-	return groupModel{
-		Name: types.StringValue(obj.Name),
-	}
 }
